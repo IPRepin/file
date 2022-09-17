10cdb77edb64aafb7a3ed145a405e4dad22f4e77
Pavel Repin <sghcd2012@yandex.ru>
2022-09-17 13:15:34 +0300

    read methods

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ read_data_txt = file_data_txt.read() #чтение файла
 
 file_data_txt.close() #закрытие файла
 
-# ускоряем работу с файлами припомощи контентного менеджера with
+# ускоряем работу с файлами при помощи контентного менеджера with
 with open('data.txt', encoding='utf8') as file_data_txt: #with после прочтения файла сам его закрывает
     read_data_txt = file_data_txt.read()
     # print(read_data_txt)
@@ -14,12 +14,17 @@ with open('data.txt', encoding='utf8') as file_data_txt: #with после про
 # методы чтения файла
 with open('data.txt', encoding='utf8') as file_data_txt: #read считывает весь файл целиком (class str)
     read_data_txt = file_data_txt.read()
-    print(read_data_txt)
+    # print(read_data_txt)
+    # print()
 
 with open('data.txt', 'r', encoding='utf8') as file_data_txt: #'r' явно указываем режим чтения
     read_data_txt = file_data_txt.readline() #readline считывет за раз только одну строку (class str)
-    print(read_data_txt)
+    # print(read_data_txt)
 
 with open('data.txt', encoding='utf8') as file_data_txt:
     read_data_txt = file_data_txt.readlines() #readlines считывет весь файл, но посторочно помещаяя каждую строку в список (class list)
-    print(read_data_txt)
+    # print(read_data_txt)
+
+with open('data.txt', 'r', encoding='utf8') as file_data_txt:
+    for line in file_data_txt:
+        print(line)
