72bc2a6fcfe5ad914fa95523f17eec58ca15a3ba
Pavel Repin <sghcd2012@yandex.ru>
2022-09-18 17:49:23 +0300

    complit v2.py

--- v2.py
+++ v2.py
@@ -4,26 +4,26 @@ def repeat():
         if repeat_inn == 'y':
             def name_department():
                 with open('personnel.txt', 'a') as department:
-                    dep = department.write(input('Ведите название отдела:'))
+                    dep = department.write(f'Название отдела: {input("Ведите название отдела: ")}\n')
                     return dep
 
             def number_of_staff_department():
                 with open('personnel.txt', 'a') as number_of_staff:
-                    nos = number_of_staff.write(number_of_staff_add)
+                    nos = number_of_staff.write(f'Колличество сотрудников: {number_of_staff_add}\n')
                 return nos
 
             def add_worker():
                 with open('personnel.txt', 'a') as add_worker_in:
-                    dict = {}
                     for age_spec in range(1, number_of_staff_add + 1):
-                        name = input('Введите имя сотрудника: ')
-                        age_spec = {input('Введите возраст сотрудника: '), input('Введите специальность сотрудника: ')}
-                        dict[name] = age_spec
-                        add_sp = add_worker_in.writelines(f'{dict}\n')
-                return add_sp
+                        name = f'{input("Введите имя сотрудника: ")}\n'
+                        age_worker = f'Возраст: {input("Введите возраст сотрудника: ")} лет,'
+                        spec_worker = f'Специальность: {input("Введите специальность сотрудника: ")}'
+                        add_sp = add_worker_in.writelines(f'{name}{age_worker} | {spec_worker}\n')
+                    return add_sp
 
             name_department()
-            number_of_staff_add = input('Ведите колличество работников: ')
+            number_of_staff_add = input('Ведите колличество сотрудников: ')
+            
             number_of_staff_department()
             number_of_staff_add = int(number_of_staff_add)
             add_worker()
